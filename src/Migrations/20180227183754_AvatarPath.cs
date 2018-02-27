7f7033267703156011ef1f0dcf7a4966076a5dba
filedesless <filedesless@gmail.com>
2018-02-27 13:39:57 -0500

    DB migration

---
+++ src/Migrations/20180227183754_AvatarPath.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HTLLBB.Migrations
+{
+    public partial class AvatarPath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarPath",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarPath",
+                table: "AspNetUsers");
+        }
+    }
+}
--- src/Models/ApplicationUser.cs
+++ src/Models/ApplicationUser.cs
@@ -9,5 +9,6 @@ namespace HTLLBB.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public String AvatarPath { get; set; }
     }
 }
