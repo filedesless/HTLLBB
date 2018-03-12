07d2ba7d1d2079154e80f7f6a8e391fb1ad38022
filedesless <filedesless@gmail.com>
2018-03-12 10:07:56 -0400

    added time to chatbox

--- src/Hubs/ChatHub.cs
+++ src/Hubs/ChatHub.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
+using CommonMark;
+using Ganss.XSS;
 using HTLLBB.Data;
 using HTLLBB.Services;
 using Microsoft.AspNetCore.Authorization;
@@ -14,7 +16,9 @@ namespace HTLLBB.Hubs
     {
         ConnectionMultiplexer _redis;
         IDatabase _db;
-        String _msgKey = "chatbox_msg", _usrKey = "chatbox_user";
+        String  _msgKey = "chatbox_msg", 
+                _usrKey = "chatbox_user",
+                _timeKey = "chatbox_time";
         long _maxLength = 1024;
         ApplicationDbContext _ctx;
 
@@ -29,13 +33,14 @@ namespace HTLLBB.Hubs
         public async Task RetrieveMessages()
         {
             var users = await _db.ListRangeAsync(_usrKey);
+            var times = await _db.ListRangeAsync(_timeKey);
             var messages = await _db.ListRangeAsync(_msgKey);
 
             for (int i = 0; i < users.Length && i < messages.Length; ++i)
             {
                 String userName = users[i].ToString();
                 var user = await _ctx.Users.SingleOrDefaultAsync(u => u.UserName == userName);
-				await Clients.All.InvokeAsync("Send", userName, user.AvatarPath, messages[i].ToString());
+                await Clients.All.InvokeAsync("Send", userName, user.AvatarPath, times[i].ToString(), messages[i].ToString());
             }
         }
 
@@ -44,19 +49,23 @@ namespace HTLLBB.Hubs
         {
             long n = _db.ListLength(_usrKey);
 
+            var curTime = DateTime.UtcNow.ToString();
+
             await _db.ListRightPushAsync(_usrKey, Context.User.Identity.Name);
+            await _db.ListRightPushAsync(_timeKey, curTime);
             await _db.ListRightPushAsync(_msgKey, message);
 
             if (n >= _maxLength)
             {
                 await _db.ListLeftPopAsync(_usrKey);
+                await _db.ListLeftPopAsync(_timeKey);
                 await _db.ListLeftPopAsync(_msgKey);
             }
 
             String userName = Context.User.Identity.Name;
             var user = await _ctx.Users.SingleOrDefaultAsync(u => u.UserName == userName);
 
-            return Clients.All.InvokeAsync("Send", userName, user.AvatarPath, message);
+            return Clients.All.InvokeAsync("Send", userName, user.AvatarPath, curTime, message);
         }
     }
 }
